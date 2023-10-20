11f60a0b63c3998d8101ac5b8dcda0bb2a330383
Ivo Kovacevic <ikovacevic96k@gmail.com>
2023-10-20 09:58:41 +0200

    Change ec2 security group to allow only https

--- src/components/ec2-ssm-connect.ts
+++ src/components/ec2-ssm-connect.ts
@@ -34,21 +34,9 @@ export class Ec2SSMConnect extends pulumi.ComponentResource {
         ingress: [
           {
             protocol: 'tcp',
-            fromPort: 22,
-            toPort: 22,
-            cidrBlocks: ['0.0.0.0/0'],
-          },
-          {
-            protocol: 'tcp',
-            fromPort: 80,
-            toPort: 80,
-            cidrBlocks: ['0.0.0.0/0'],
-          },
-          {
-            protocol: 'tcp',
             fromPort: 443,
             toPort: 443,
-            cidrBlocks: ['0.0.0.0/0'],
+            cidrBlocks: [args.vpc.vpc.cidrBlock],
           },
         ],
         egress: [
