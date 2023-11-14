a0056e95007b295cc9495dda42f85c90c9c2448c
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-14 16:45:28 +0100

    Extracted web server common logic

--- src/components/web-server.ts
+++ src/components/web-server.ts
@@ -1,26 +1,34 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
-import { CustomSize } from '../types/size';
-import { PredefinedSize, commonTags } from '../constants';
-import { ContainerDefinition } from '@pulumi/aws/ecs';
+import * as awsx from '@pulumi/awsx';
+import { commonTags } from '../constants';
 import { AcmCertificate } from './acm-certificate';
-import {
-  EcsServiceArgs,
-  assumeRolePolicy,
-  awsRegion,
-  defaults,
-} from './ecs-service';
+import { EcsService, defaults } from './ecs-service';
+import { Size } from '../types/size';
 
-export type WebServerArgs = EcsServiceArgs & {
+export type WebServerArgs = {
+  /**
+   * The ECR image used to start a container.
+   */
+  image: pulumi.Input<string>;
+  /*
+   * Exposed service port.
+   */
+  port: pulumi.Input<number>;
   /**
    * The domain which will be used to access the service.
    * The domain or subdomain must belong to the provided hostedZone.
    */
   domain: pulumi.Input<string>;
   /**
-   * The ID of the hosted zone.
+   * CPU and memory size used for running the container. Defaults to "small".
+   * Available predefined options are:
+   * - small (0.25 vCPU, 0.5 GB memory)
+   * - medium (0.5 vCPU, 1 GB memory)
+   * - large (1 vCPU memory, 2 GB memory)
+   * - xlarge (2 vCPU, 4 GB memory)
    */
-  hostedZoneId: pulumi.Input<string>;
+  size?: pulumi.Input<Size>;
   /**
    * Number of instances of the task definition to place and keep running. Defaults to 1.
    */
@@ -34,6 +42,29 @@ export type WebServerArgs = EcsServiceArgs & {
    */
   maxCount?: pulumi.Input<number>;
   /**
+   * The aws.ecs.Cluster resource.
+   */
+  cluster: aws.ecs.Cluster;
+  /**
+   * The awsx.ec2.Vpc resource.
+   */
+  vpc: awsx.ec2.Vpc;
+  /**
+   * The environment variables to pass to a container. Don't use this field for
+   * sensitive information such as passwords, API keys, etc. For that purpose,
+   * please use the `secrets` property.
+   * Defaults to [].
+   */
+  environment?: aws.ecs.KeyValuePair[];
+  /**
+   * The secrets to pass to the container. Defaults to [].
+   */
+  secrets?: aws.ecs.Secret[];
+  /**
+   * The ID of the hosted zone.
+   */
+  hostedZoneId: pulumi.Input<string>;
+  /**
    * Path for the health check request. Defaults to "/healtcheck".
    */
   healtCheckPath?: pulumi.Input<string>;
@@ -41,15 +72,13 @@ export type WebServerArgs = EcsServiceArgs & {
 
 export class WebServer extends pulumi.ComponentResource {
   name: string;
-  logGroup: aws.cloudwatch.LogGroup;
+  service: EcsService;
   certificate: AcmCertificate;
   lbSecurityGroup: aws.ec2.SecurityGroup;
   lb: aws.lb.LoadBalancer;
   lbTargetGroup: aws.lb.TargetGroup;
   lbHttpListener: aws.lb.Listener;
   lbTlsListener: aws.lb.Listener;
-  taskDefinition: aws.ecs.TaskDefinition;
-  service: aws.ecs.Service;
 
   constructor(
     name: string,
@@ -58,10 +87,23 @@ export class WebServer extends pulumi.ComponentResource {
   ) {
     super('studion:WebServer', name, args, opts);
 
-    this.name = name;
-    this.logGroup = this.createLogGroup();
+    const {
+      image,
+      port,
+      size,
+      cluster,
+      vpc,
+      environment,
+      secrets,
+      healtCheckPath,
+      domain,
+      hostedZoneId,
+      desiredCount,
+      minCount,
+      maxCount,
+    } = args;
 
-    const { domain, hostedZoneId, vpc, port, healtCheckPath } = args;
+    this.name = name;
     this.certificate = this.createTlsCertificate({ domain, hostedZoneId });
     const {
       lb,
@@ -75,25 +117,57 @@ export class WebServer extends pulumi.ComponentResource {
     this.lbHttpListener = lbHttpListener;
     this.lbTlsListener = lbTlsListener;
     this.lbSecurityGroup = lbSecurityGroup;
-    this.taskDefinition = this.createTaskDefinition(args);
-    this.service = this.createEcsService(args);
-    this.createDnsRecord({ domain, hostedZoneId });
-    this.enableAutoscaling(args);
 
-    this.registerOutputs();
-  }
-
-  private createLogGroup() {
-    const logGroup = new aws.cloudwatch.LogGroup(
-      `${this.name}-log-group`,
+    const securityGroup = new aws.ec2.SecurityGroup(
+      `${name}-security-group`,
       {
-        retentionInDays: 14,
-        namePrefix: `/ecs/${this.name}-`,
+        vpcId: vpc.vpcId,
+        ingress: [
+          {
+            fromPort: 0,
+            toPort: 0,
+            protocol: '-1',
+            securityGroups: [this.lbSecurityGroup.id],
+          },
+        ],
+        egress: [
+          {
+            fromPort: 0,
+            toPort: 0,
+            protocol: '-1',
+            cidrBlocks: ['0.0.0.0/0'],
+          },
+        ],
         tags: commonTags,
       },
       { parent: this },
     );
-    return logGroup;
+
+    this.service = new EcsService(
+      name,
+      {
+        image,
+        port,
+        cluster,
+        ...(desiredCount && { desiredCount }),
+        ...(minCount && { minCount }),
+        ...(maxCount && { maxCount }),
+        ...(size && { size }),
+        environment,
+        secrets,
+        enableServiceAutoDiscovery: false,
+        enableAutoScaling: true,
+        lbTargetGroupArn: lbTargetGroup.arn,
+        assignPublicIp: true,
+        vpc,
+        securityGroup,
+      },
+      { ...opts, parent: this },
+    );
+
+    this.createDnsRecord({ domain, hostedZoneId });
+
+    this.registerOutputs();
   }
 
   private createTlsCertificate({
@@ -229,219 +303,6 @@ export class WebServer extends pulumi.ComponentResource {
     };
   }
 
-  private createTaskDefinition(args: WebServerArgs) {
-    const argsWithDefaults = Object.assign({}, defaults, args);
-    const stack = pulumi.getStack();
-
-    const secretManagerSecretsInlinePolicy = {
-      name: `${this.name}-secret-manager-access`,
-      policy: JSON.stringify({
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Sid: 'AllowContainerToGetSecretManagerSecrets',
-            Effect: 'Allow',
-            Action: ['secretsmanager:GetSecretValue'],
-            Resource: '*',
-          },
-        ],
-      }),
-    };
-
-    const taskExecutionRole = new aws.iam.Role(
-      `${this.name}-ecs-task-exec-role`,
-      {
-        namePrefix: `${this.name}-ecs-task-exec-role-`,
-        assumeRolePolicy,
-        managedPolicyArns: [
-          'arn:aws:iam::aws:policy/CloudWatchFullAccess',
-          'arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess',
-        ],
-        inlinePolicies: [
-          secretManagerSecretsInlinePolicy,
-          ...argsWithDefaults.taskExecutionRoleInlinePolicies,
-        ],
-        tags: commonTags,
-      },
-      { parent: this },
-    );
-
-    const execCmdInlinePolicy = {
-      name: `${this.name}-ecs-exec`,
-      policy: JSON.stringify({
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Sid: 'AllowContainerToCreateECSExecSSMChannel',
-            Effect: 'Allow',
-            Action: [
-              'ssmmessages:CreateControlChannel',
-              'ssmmessages:CreateDataChannel',
-              'ssmmessages:OpenControlChannel',
-              'ssmmessages:OpenDataChannel',
-            ],
-            Resource: '*',
-          },
-        ],
-      }),
-    };
-
-    const taskRole = new aws.iam.Role(
-      `${this.name}-ecs-task-role`,
-      {
-        namePrefix: `${this.name}-ecs-task-role-`,
-        assumeRolePolicy,
-        inlinePolicies: [
-          execCmdInlinePolicy,
-          ...argsWithDefaults.taskRoleInlinePolicies,
-        ],
-        tags: commonTags,
-      },
-      { parent: this },
-    );
-
-    const parsedSize = pulumi.all([argsWithDefaults.size]).apply(([size]) => {
-      const mapCapabilities = ({ cpu, memory }: CustomSize) => ({
-        cpu: String(cpu),
-        memory: String(memory),
-      });
-      if (typeof size === 'string') {
-        return mapCapabilities(PredefinedSize[size]);
-      }
-      if (typeof size === 'object') {
-        return mapCapabilities(size);
-      }
-      throw Error('Incorrect EcsService size argument');
-    });
-
-    const taskDefinition = new aws.ecs.TaskDefinition(
-      `${this.name}-task-definition`,
-      {
-        family: `${this.name}-task-definition-${stack}`,
-        networkMode: 'awsvpc',
-        executionRoleArn: taskExecutionRole.arn,
-        taskRoleArn: taskRole.arn,
-        cpu: parsedSize.cpu,
-        memory: parsedSize.memory,
-        requiresCompatibilities: ['FARGATE'],
-        containerDefinitions: pulumi
-          .all([
-            this.name,
-            argsWithDefaults.image,
-            argsWithDefaults.port,
-            argsWithDefaults.environment,
-            argsWithDefaults.secrets,
-            this.logGroup.name,
-            awsRegion,
-          ])
-          .apply(
-            ([
-              containerName,
-              image,
-              port,
-              environment,
-              secrets,
-              logGroup,
-              region,
-            ]) => {
-              return JSON.stringify([
-                {
-                  readonlyRootFilesystem: false,
-                  name: containerName,
-                  image,
-                  essential: true,
-                  portMappings: [
-                    {
-                      containerPort: port,
-                      protocol: 'tcp',
-                    },
-                  ],
-                  logConfiguration: {
-                    logDriver: 'awslogs',
-                    options: {
-                      'awslogs-group': logGroup,
-                      'awslogs-region': region,
-                      'awslogs-stream-prefix': 'ecs',
-                    },
-                  },
-                  environment,
-                  secrets,
-                },
-              ] as ContainerDefinition[]);
-            },
-          ),
-        tags: { ...commonTags, ...argsWithDefaults.tags },
-      },
-      { parent: this },
-    );
-
-    return taskDefinition;
-  }
-
-  private createEcsService(args: WebServerArgs) {
-    const argsWithDefaults = Object.assign({}, defaults, args);
-
-    const serviceSecurityGroup = new aws.ec2.SecurityGroup(
-      `${this.name}-security-group`,
-      {
-        vpcId: argsWithDefaults.vpc.vpcId,
-        ingress: [
-          {
-            fromPort: 0,
-            toPort: 0,
-            protocol: '-1',
-            securityGroups: [this.lbSecurityGroup.id],
-          },
-        ],
-        egress: [
-          {
-            fromPort: 0,
-            toPort: 0,
-            protocol: '-1',
-            cidrBlocks: ['0.0.0.0/0'],
-          },
-        ],
-        tags: commonTags,
-      },
-      { parent: this },
-    );
-
-    const service = new aws.ecs.Service(
-      `${this.name}-service`,
-      {
-        name: this.name,
-        cluster: argsWithDefaults.cluster.id,
-        launchType: 'FARGATE',
-        desiredCount: argsWithDefaults.desiredCount,
-        taskDefinition: this.taskDefinition.arn,
-        enableExecuteCommand: true,
-        loadBalancers: [
-          {
-            containerName: this.name,
-            containerPort: argsWithDefaults.port,
-            targetGroupArn: this.lbTargetGroup.arn,
-          },
-        ],
-        networkConfiguration: {
-          assignPublicIp: true,
-          subnets: argsWithDefaults.vpc.publicSubnetIds,
-          securityGroups: [serviceSecurityGroup.id],
-        },
-        tags: { ...commonTags, ...argsWithDefaults.tags },
-      },
-      {
-        parent: this,
-        dependsOn: [
-          this.lb,
-          this.lbTargetGroup,
-          this.lbHttpListener,
-          this.lbTlsListener,
-        ],
-      },
-    );
-    return service;
-  }
-
   private createDnsRecord({
     domain,
     hostedZoneId,
@@ -459,57 +320,6 @@ export class WebServer extends pulumi.ComponentResource {
             evaluateTargetHealth: true,
           },
         ],
-      },
-      { parent: this },
-    );
-  }
-
-  private enableAutoscaling(args: WebServerArgs) {
-    const argsWithDefaults = Object.assign({}, defaults, args);
-
-    const autoscalingTarget = new aws.appautoscaling.Target(
-      `${this.name}-autoscale-target`,
-      {
-        minCapacity: argsWithDefaults.minCount,
-        maxCapacity: argsWithDefaults.maxCount,
-        resourceId: pulumi.interpolate`service/${argsWithDefaults.cluster.name}/${this.service.name}`,
-        serviceNamespace: 'ecs',
-        scalableDimension: 'ecs:service:DesiredCount',
-        tags: commonTags,
-      },
-      { parent: this },
-    );
-
-    const memoryAutoscalingPolicy = new aws.appautoscaling.Policy(
-      `${this.name}-memory-autoscale-policy`,
-      {
-        policyType: 'TargetTrackingScaling',
-        resourceId: autoscalingTarget.resourceId,
-        scalableDimension: autoscalingTarget.scalableDimension,
-        serviceNamespace: autoscalingTarget.serviceNamespace,
-        targetTrackingScalingPolicyConfiguration: {
-          predefinedMetricSpecification: {
-            predefinedMetricType: 'ECSServiceAverageMemoryUtilization',
-          },
-          targetValue: 80,
-        },
-      },
-      { parent: this },
-    );
-
-    const cpuAutoscalingPolicy = new aws.appautoscaling.Policy(
-      `${this.name}-cpu-autoscale-policy`,
-      {
-        policyType: 'TargetTrackingScaling',
-        resourceId: autoscalingTarget.resourceId,
-        scalableDimension: autoscalingTarget.scalableDimension,
-        serviceNamespace: autoscalingTarget.serviceNamespace,
-        targetTrackingScalingPolicyConfiguration: {
-          predefinedMetricSpecification: {
-            predefinedMetricType: 'ECSServiceAverageCPUUtilization',
-          },
-          targetValue: 60,
-        },
       },
       { parent: this },
     );
