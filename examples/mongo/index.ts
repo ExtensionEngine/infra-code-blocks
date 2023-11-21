6dcadf4d2da1bef9d8e87f6a5437120fae439380
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-22 00:09:37 +0100

    Fixes

--- examples/mongo/index.ts
+++ examples/mongo/index.ts
@@ -4,21 +4,12 @@ import * as awsx from '@pulumi/awsx';
 
 require('dotenv').config();
 
-const imageRepository = new aws.ecr.Repository('web-server-ecs-repo', {
-  name: 'web-server-repo',
-  forceDelete: true,
-});
-
-const appImg = new awsx.ecr.Image('web-server-img', {
-  repositoryUrl: imageRepository.repositoryUrl,
-  context: './web-server',
-  extraOptions: ['--platform', 'linux/amd64', '--ssh', 'default'],
-});
-
-const mongoServiceName = 'mongo-example';
 const mongoPort = 27017;
+const mongoServiceName = 'mongo-example';
 const mongoUsername = process.env.MONGO_USERNAME || '';
 const mongoPassword = process.env.MONGO_PASSWORD || '';
+
+const webServerImage = createWebServerImage();
 
 const project = new Project('mongo-project', {
   services: [
@@ -34,9 +25,10 @@ const project = new Project('mongo-project', {
       type: 'WEB_SERVER',
       serviceName: 'web-server-example',
       port: 3000,
-      image: appImg.imageUri,
+      image: webServerImage.imageUri,
       domain: 'ptrutanic.gostudion.com',
       desiredCount: 1,
+      size: 'small',
       autoscaling: { enabled: false },
       environment: () => {
         return [
@@ -58,5 +50,18 @@ const project = new Project('mongo-project', {
   ],
   hostedZoneId: 'Z034266420CJCR4IBU4AG',
 });
+
+function createWebServerImage() {
+  const imageRepository = new aws.ecr.Repository('web-server-ecs-repo', {
+    name: 'web-server-repo',
+    forceDelete: true,
+  });
+
+  return new awsx.ecr.Image('web-server-img', {
+    repositoryUrl: imageRepository.repositoryUrl,
+    context: './web-server',
+    extraOptions: ['--platform', 'linux/amd64', '--ssh', 'default'],
+  });
+}
 
 export default project.name;
