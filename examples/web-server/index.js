46a89783d3266fbcc935482898a2965b48f593ce
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-21 16:16:49 +0100

    Added web server

---
+++ examples/web-server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send(`Hello world.`);
+});
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000');
+});
