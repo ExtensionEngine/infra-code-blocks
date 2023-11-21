f6c79ced2d42df3f13cf63f9f9b12380da2858a6
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-21 19:19:06 +0100

    Added web server

--- examples/mongo/index.ts
+++ examples/mongo/index.ts
@@ -2,33 +2,48 @@ import { Project } from '@studion/infra-code-blocks';
 import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 
-const imageRepository = new aws.ecr.Repository('example-ecs-repo', {
+const imageRepository = new aws.ecr.Repository('web-server-ecs-repo', {
+  name: 'web-server-repo',
   forceDelete: true,
 });
 
-const appImg = new awsx.ecr.Image('app-img', {
+const appImg = new awsx.ecr.Image('web-server-img', {
   repositoryUrl: imageRepository.repositoryUrl,
-  dockerfile: '',
+  context: './web-server',
+  extraOptions: ['--platform', 'linux/amd64', '--ssh', 'default'],
 });
+
+const mongoServiceName = 'mongo-example';
 
 const project = new Project('mongo-project', {
   services: [
     {
       type: 'MONGO',
-      serviceName: 'mongo-example',
+      serviceName: mongoServiceName,
       port: 27017,
       username: 'admin',
       password: 'admin',
       size: 'small',
     },
-    // {
-    //   type: 'WEB_SERVER',
-    //   serviceName: 't_web_server',
-    //   port: 27017,
-    //   image: 'mongo:latest',
-    //   domain: 'ptrutanic.gostudion.com',
-    // },
+    {
+      type: 'WEB_SERVER',
+      serviceName: 'web-server-example',
+      port: 3000,
+      image: appImg.imageUri,
+      domain: 'ptrutanic.gostudion.com',
+      desiredCount: 1,
+      autoscaling: { enabled: false },
+      environment: () => {
+        return [
+          {
+            name: 'MONGO_URL',
+            value: `${mongoServiceName}.${mongoServiceName}`,
+          },
+        ];
+      },
+    },
   ],
+  hostedZoneId: 'Z034266420CJCR4IBU4AG',
 });
 
 export default project.name;
