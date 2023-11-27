f6bf53beb957aed94ebd8c6043d06e9fc18d439b
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-27 20:30:19 +0100

    Added aws cert

--- examples/database-with-web-server/web-server/src/mikro-orm.config.ts
+++ examples/database-with-web-server/web-server/src/mikro-orm.config.ts
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 import { defineConfig } from '@mikro-orm/postgresql';
 import { Post } from './database/entities/post.entity';
+import { readFileSync } from 'fs';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   entities: [Post],
@@ -15,4 +18,11 @@ export default defineConfig({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   type: 'postgresql',
+  driverOptions: isProd
+    ? {
+        connection: {
+          ssl: { ca: readFileSync('./src/eu-north-1-bundle.pem') },
+        },
+      }
+    : undefined,
 });
