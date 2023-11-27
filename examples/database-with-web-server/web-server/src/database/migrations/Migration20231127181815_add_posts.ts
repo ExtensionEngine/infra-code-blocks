92b4af6c89e9d8a90daa0bdfd4c5e6440ae2d123
Petar Trutanic <ptrutanic@extensionengine.com>
2023-11-27 20:49:09 +0100

    Added seed data

--- examples/database-with-web-server/web-server/src/database/migrations/Migration20231127181815_add_posts.ts
+++ examples/database-with-web-server/web-server/src/database/migrations/Migration20231127181815_add_posts.ts
@@ -1,4 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
+import { posts } from '../seed/posts';
+import { Post } from '../entities/post.entity';
 
 const TABLE_NAME = 'posts';
 
@@ -6,22 +8,23 @@ export class Migration20231127181815_add_posts extends Migration {
   async up(): Promise<void> {
     const knex = this.getKnex();
 
-    const createPostsTable = knex.schema.createTable(TABLE_NAME, table => {
+    await knex.schema.createTable(TABLE_NAME, table => {
       table.increments('id').primary();
       table.string('name').notNullable();
-      table.string('content').notNullable();
+      table.string('content', 9999).notNullable();
       table
         .timestamp('createdAt', { useTz: true })
         .notNullable()
         .defaultTo(knex.fn.now());
     });
 
-    this.addSql(createPostsTable.toQuery());
+    const data = posts.map(post => new Post(post.name, post.content));
+    return knex.batchInsert(TABLE_NAME, data);
   }
 
   async down(): Promise<void> {
     const knex = this.getKnex();
 
-    this.addSql(knex.schema.dropTable(TABLE_NAME).toQuery());
+    return knex.schema.dropTable(TABLE_NAME);
   }
 }
