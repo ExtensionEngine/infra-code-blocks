557df584ce3b5542c320bbed767585a4ef28de74
Miro Dojkic <mdojkic@gmail.com>
2025-01-25 23:17:21 +0100

    Add spinner

--- tests/automation.ts
+++ tests/automation.ts
@@ -4,20 +4,25 @@ import {
   LocalWorkspace,
   OutputMap
 } from '@pulumi/pulumi/automation';
+import { createSpinner } from 'nanospinner';
 
 export async function deploy(args: LocalProgramArgs): Promise<OutputMap> {
+  const spinner = createSpinner('Deploying stack...').start();
   const stack = await LocalWorkspace.createOrSelectStack(args);
   await stack.setConfig('aws:region', { value: 'us-east-2' });
   const up = await stack.up({ logToStdErr: true });
+  spinner.success({ text: 'Stack deployed' });
 
-  console.info('Deploying stack...');
   return up.outputs;
 }
 
 export async function destroy(args: LocalProgramArgs): Promise<DestroyResult> {
+  const spinner = createSpinner('Destroying stack...').start();
   const stack = await LocalWorkspace.createOrSelectStack(args);
-  console.log('Destroying stack...');
-  return stack.destroy();
+  const result = await stack.destroy();
+  spinner.success({ text: 'Stack destroyed' });
+
+  return result;
 }
 
 export async function getOutputs(args: LocalProgramArgs): Promise<OutputMap> {
