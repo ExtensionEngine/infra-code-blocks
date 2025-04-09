9eabd6c679b89732b4c9407152e96c3e2b48cd5a
droguljic <1875821+droguljic@users.noreply.github.com>
2025-04-09 17:30:36 +0200

    test: remove web server docker build step
    By using existing image from the docker hub, `nginxdemos/nginx-hello`.
    Removes web server's test app since it is no longer required.
    
    The web server healthcheck endpoint test is updated to only test for
    status code.

--- tests/web-server/infrastructure/index.ts
+++ tests/web-server/infrastructure/index.ts
@@ -4,7 +4,6 @@ import * as awsx from '@pulumi/awsx';
 import * as pulumi from '@pulumi/pulumi';
 import * as path from 'pathe';
 
-const webServerImage = createWebServerImage();
 const serviceName = 'web-server-example';
 const stackName = pulumi.getStack();
 const project: Project = new Project('web-server-test', { services: [] });
@@ -19,26 +18,11 @@ const webServer = new studion.WebServer(serviceName, {
   cluster,
   vpc: project.vpc,
   publicSubnetIds: project.vpc.publicSubnetIds,
-  port: 3000,
-  image: webServerImage.imageUri,
+  port: 8080,
+  image: 'nginxdemos/nginx-hello:plain-text',
   desiredCount: 1,
   size: 'small',
   autoscaling: { enabled: false }
 });
 
 export { project, webServer };
-
-function createWebServerImage() {
-  const imageRepository = new aws.ecr.Repository('repository', {
-    forceDelete: true,
-  });
-
-  const targetPath = path.join(__dirname, '..', 'app');
-  return new awsx.ecr.Image('app', {
-    repositoryUrl: imageRepository.repositoryUrl,
-    context: targetPath,
-    platform: 'linux/amd64',
-    extraOptions: ['--platform', 'linux/amd64', '--ssh', 'default']
-  } as awsx.ecr.ImageArgs);
-}
-
