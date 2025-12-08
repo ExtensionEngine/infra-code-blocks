65d08cb4f3fa30bca8a53d50910b5da6aa1fba04
mandryllo <frano.sincic@gostudion.com>
2025-12-08 11:49:43 +0100

    Add default db tests

---
+++ tests/database/index.test.ts
@@ -0,0 +1,40 @@
+import { describe, before, after } from 'node:test';
+import * as automation from '../automation';
+import * as config from './infrastructure/config';
+import { DatabaseTestContext } from './test-context';
+import { EC2Client } from '@aws-sdk/client-ec2';
+import { InlineProgramArgs } from '@pulumi/pulumi/automation';
+import { KMSClient } from '@aws-sdk/client-kms';
+import { RDSClient } from '@aws-sdk/client-rds';
+import { testDefaultDb } from './default-db.test';
+
+const programArgs: InlineProgramArgs = {
+  stackName: 'dev',
+  projectName: 'icb-test-database',
+  program: () => import('./infrastructure'),
+};
+
+describe('Database component deployment', () => {
+  const region = process.env.AWS_REGION;
+  if (!region) {
+    throw new Error('AWS_REGION environment variable is required');
+  }
+
+  const ctx: DatabaseTestContext = {
+    outputs: {},
+    config,
+    clients: {
+      rds: new RDSClient({ region }),
+      ec2: new EC2Client({ region }),
+      kms: new KMSClient({ region }),
+    },
+  };
+
+  before(async () => {
+    ctx.outputs = await automation.deploy(programArgs);
+  });
+
+  after(() => automation.destroy(programArgs));
+
+  describe('Default database', () => testDefaultDb(ctx));
+});
---
+++ tests/database/test-context.ts
@@ -0,0 +1,37 @@
+import { EC2Client } from '@aws-sdk/client-ec2';
+import { KMSClient } from '@aws-sdk/client-kms';
+import { OutputMap } from '@pulumi/pulumi/automation';
+import { RDSClient } from '@aws-sdk/client-rds';
+
+interface ConfigContext {
+  config: DatabaseTestConfig;
+}
+
+interface DatabaseTestConfig {
+  appName: string;
+  stackName: string;
+  tags: {
+    Project: string;
+    Environment: string;
+  };
+  dbName: string;
+  dbUsername: string;
+  dbPassword: string;
+}
+
+interface PulumiProgramContext {
+  outputs: OutputMap;
+}
+
+interface AwsContext {
+  clients: {
+    rds: RDSClient;
+    ec2: EC2Client;
+    kms: KMSClient;
+  };
+}
+
+export interface DatabaseTestContext
+  extends ConfigContext,
+    PulumiProgramContext,
+    AwsContext {}
