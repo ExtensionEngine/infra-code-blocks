ca2bac5bac2fb7b55e1a1648208a19a36d45b7b6
Borna Šunjić <borna.sunjic.st@gmail.com>
2026-01-13 15:48:04 +0100

    test: replace `Project` component in test infrastructure (#112)
    * test: replace project with vpc component
    
    * refactor: usage of vpc output in tests

--- tests/ecs-service/infrastructure/index.ts
+++ tests/ecs-service/infrastructure/index.ts
@@ -15,7 +15,12 @@ const sampleServiceContainer = {
   portMappings: [studion.EcsService.createTcpPortMapping(appPort)],
 };
 
-const project = new Project(appName, { services: [] });
+const parent = new pulumi.ComponentResource(
+  'studion:ecs:TestGroup',
+  `${appName}-root`,
+);
+
+const vpc = new studion.Vpc(`${appName}-vpc`, {}, { parent });
 
 const cluster = new aws.ecs.Cluster(
   `${appName}-cluster`,
@@ -23,100 +28,128 @@ const cluster = new aws.ecs.Cluster(
     name: `${appName}-cluster-${stackName}`,
     tags,
   },
-  { parent: project },
+  { parent },
 );
 
-const minimalEcsService = new studion.EcsService(`${appName}-min`, {
-  cluster,
-  vpc: project.vpc,
-  containers: [sampleServiceContainer],
-  tags,
-});
-
-const lbSecurityGroup = new aws.ec2.SecurityGroup(`${appName}-lb-sg`, {
-  vpcId: project.vpc.vpcId,
-  ingress: [
-    {
-      protocol: 'tcp',
-      fromPort: appPort,
-      toPort: appPort,
-      cidrBlocks: ['0.0.0.0/0'],
-    },
-  ],
-  egress: [
-    {
-      protocol: '-1',
-      fromPort: 0,
-      toPort: 0,
-      cidrBlocks: ['0.0.0.0/0'],
-    },
-  ],
-  tags,
-});
-
-const lb = new aws.lb.LoadBalancer(`${appName}-lb`, {
-  internal: false,
-  loadBalancerType: 'application',
-  securityGroups: [lbSecurityGroup.id],
-  subnets: project.vpc.publicSubnetIds,
-  tags,
-});
-
-const targetGroup = new aws.lb.TargetGroup(`${appName}-tg`, {
-  port: appPort,
-  protocol: 'HTTP',
-  targetType: 'ip',
-  vpcId: project.vpc.vpcId,
-  healthCheck: {
-    path: '/',
-    port: 'traffic-port',
+const minimalEcsService = new studion.EcsService(
+  `${appName}-min`,
+  {
+    cluster,
+    vpc: vpc.vpc,
+    containers: [sampleServiceContainer],
+    tags,
   },
-  tags,
-});
+  { parent },
+);
 
-const listener = new aws.lb.Listener(`${appName}-listener`, {
-  loadBalancerArn: lb.arn,
-  port: appPort,
-  protocol: 'HTTP',
-  defaultActions: [
-    {
-      type: 'forward',
-      targetGroupArn: targetGroup.arn,
-    },
-  ],
-  tags,
-});
+const lbSecurityGroup = new aws.ec2.SecurityGroup(
+  `${appName}-lb-sg`,
+  {
+    vpcId: vpc.vpc.vpcId,
+    ingress: [
+      {
+        protocol: 'tcp',
+        fromPort: appPort,
+        toPort: appPort,
+        cidrBlocks: ['0.0.0.0/0'],
+      },
+    ],
+    egress: [
+      {
+        protocol: '-1',
+        fromPort: 0,
+        toPort: 0,
+        cidrBlocks: ['0.0.0.0/0'],
+      },
+    ],
+    tags,
+  },
+  { parent },
+);
 
-const ecsServiceWithLb = new studion.EcsService(`${appName}-lb`, {
-  cluster,
-  vpc: project.vpc,
-  containers: [sampleServiceContainer],
-  assignPublicIp: true,
-  loadBalancers: [
-    {
-      containerName: 'sample-service',
-      containerPort: appPort,
-      targetGroupArn: targetGroup.arn,
+const lb = new aws.lb.LoadBalancer(
+  `${appName}-lb`,
+  {
+    internal: false,
+    loadBalancerType: 'application',
+    securityGroups: [lbSecurityGroup.id],
+    subnets: vpc.vpc.publicSubnetIds,
+    tags,
+  },
+  { parent },
+);
+
+const targetGroup = new aws.lb.TargetGroup(
+  `${appName}-tg`,
+  {
+    port: appPort,
+    protocol: 'HTTP',
+    targetType: 'ip',
+    vpcId: vpc.vpc.vpcId,
+    healthCheck: {
+      path: '/',
+      port: 'traffic-port',
     },
-  ],
-  tags,
-});
+    tags,
+  },
+  { parent },
+);
+
+const listener = new aws.lb.Listener(
+  `${appName}-listener`,
+  {
+    loadBalancerArn: lb.arn,
+    port: appPort,
+    protocol: 'HTTP',
+    defaultActions: [
+      {
+        type: 'forward',
+        targetGroupArn: targetGroup.arn,
+      },
+    ],
+    tags,
+  },
+  { parent },
+);
+
+const ecsServiceWithLb = new studion.EcsService(
+  `${appName}-lb`,
+  {
+    cluster,
+    vpc: vpc.vpc,
+    containers: [sampleServiceContainer],
+    assignPublicIp: true,
+    loadBalancers: [
+      {
+        containerName: 'sample-service',
+        containerPort: appPort,
+        targetGroupArn: targetGroup.arn,
+      },
+    ],
+    tags,
+  },
+  { parent },
+);
 
 const lbUrl = pulumi.interpolate`http://${lb.dnsName}`;
 
-const ecsWithDiscovery = new studion.EcsService(`${appName}-sd`, {
-  cluster,
-  vpc: project.vpc,
-  containers: [sampleServiceContainer],
-  enableServiceAutoDiscovery: true,
-  tags,
-});
+const ecsWithDiscovery = new studion.EcsService(
+  `${appName}-sd`,
+  {
+    cluster,
+    vpc: vpc.vpc,
+    containers: [sampleServiceContainer],
+    enableServiceAutoDiscovery: true,
+    tags,
+  },
+  { parent },
+);
 
 const ecsServiceWithAutoscaling = new studion.EcsService(
   `${appName}-autoscale`,
   {
     cluster,
-    vpc: project.vpc,
+    vpc: vpc.vpc,
     containers: [sampleServiceContainer],
     autoscaling: {
       enabled: true,
@@ -125,67 +158,74 @@ const ecsServiceWithAutoscaling = new studion.EcsService(
     },
     tags,
   },
+  { parent },
 );
 
-const ecsServiceWithStorage = new studion.EcsService(`${appName}-storage`, {
-  cluster,
-  vpc: project.vpc,
-  volumes: [{ name: 'data-volume' }],
-  containers: [
-    {
-      name: 'test-container',
-      image: 'amazonlinux:2',
-      portMappings: [studion.EcsService.createTcpPortMapping(appPort)],
-      mountPoints: [
-        {
-          sourceVolume: 'data-volume',
-          containerPath: '/data',
-        },
-      ],
-      environment: [{ name: 'TEST_FILE', value: '/data/test.txt' }],
-      // Enables testing EFS functionality based on logs.
-      // Command writes to EFS, then reads from it.
-      command: [
-        'sh',
-        '-c',
-        "echo 'EFS test content' > $TEST_FILE && " +
-          "if [ -f $TEST_FILE ] && grep 'EFS test content' $TEST_FILE; then " +
-          "echo 'Successfully wrote to and read from EFS volume'; " +
-          'else ' +
-          "echo 'Failed to write to or read from EFS volume'; " +
-          'exit 1; ' +
-          'fi && ' +
-          'while true; do sleep 30; done',
-      ],
-    },
-  ],
-  tags,
-});
+const ecsServiceWithStorage = new studion.EcsService(
+  `${appName}-storage`,
+  {
+    cluster,
+    vpc: vpc.vpc,
+    volumes: [{ name: 'data-volume' }],
+    containers: [
+      {
+        name: 'test-container',
+        image: 'amazonlinux:2',
+        portMappings: [studion.EcsService.createTcpPortMapping(appPort)],
+        mountPoints: [
+          {
+            sourceVolume: 'data-volume',
+            containerPath: '/data',
+          },
+        ],
+        environment: [{ name: 'TEST_FILE', value: '/data/test.txt' }],
+        // Enables testing EFS functionality based on logs.
+        // Command writes to EFS, then reads from it.
+        command: [
+          'sh',
+          '-c',
+          "echo 'EFS test content' > $TEST_FILE && " +
+            "if [ -f $TEST_FILE ] && grep 'EFS test content' $TEST_FILE; then " +
+            "echo 'Successfully wrote to and read from EFS volume'; " +
+            'else ' +
+            "echo 'Failed to write to or read from EFS volume'; " +
+            'exit 1; ' +
+            'fi && ' +
+            'while true; do sleep 30; done',
+        ],
+      },
+    ],
+    tags,
+  },
+  { parent },
+);
 
 const ecsServiceWithEmptyVolumes = new studion.EcsService(
   `${appName}-empty-vol`,
   {
     cluster,
-    vpc: project.vpc,
+    vpc: vpc.vpc,
     containers: [sampleServiceContainer],
     volumes: [],
     tags,
   },
+  { parent },
 );
 
 const ecsServiceWithOutputEmptyVolumes = new studion.EcsService(
   `${appName}-empty-otp-vol`,
   {
     cluster,
-    vpc: project.vpc,
+    vpc: vpc.vpc,
     containers: [sampleServiceContainer],
     volumes: pulumi.output([]),
     tags,
   },
+  { parent },
 );
 
 module.exports = {
-  project,
+  vpc,
   cluster,
   minimalEcsService,
   ecsServiceWithLb,
