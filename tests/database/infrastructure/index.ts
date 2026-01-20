6656b5e9fe98f0dcd5ae9969f13fcb97a1bb5d7c
mandryllo <frano.sincic@gostudion.com>
2026-01-20 10:44:57 +0100

    test: add replica db to infra

--- tests/database/infrastructure/index.ts
+++ tests/database/infrastructure/index.ts
@@ -92,6 +92,17 @@ const snapshotDb = snapshot.apply(snapshot => {
     .build({ parent });
 });
 
+const replicaDb = new studion.DatabaseBuilder(`${config.appName}-replica`)
+  .withInstance({
+    dbName: config.dbName,
+  })
+  .withCredentials({
+    username: config.dbUsername,
+  })
+  .withReplica()
+  .withVpc(vpc.vpc)
+  .build({ parent });
+
 export {
   vpc,
   defaultDb,
@@ -100,4 +111,5 @@ export {
   configurableDb,
   snapshot,
   snapshotDb,
+  replicaDb,
 };
