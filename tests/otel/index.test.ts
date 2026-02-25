0649ed3f5e1e472b4482e039b81cca7a55fccfd2
Borna Šunjić <borna.sunjic.st@gmail.com>
2026-02-25 12:30:49 +0100

    test: fix pipeline configuration test assertion

--- tests/otel/index.test.ts
+++ tests/otel/index.test.ts
@@ -354,6 +354,7 @@ describe('OtelCollectorConfigBuilder', () => {
           region: awsRegion,
           log_group_name: logGroupName,
           log_stream_name: logStreamName,
+          log_retention: logRetention,
         },
       },
       extensions: {
