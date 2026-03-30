8c11b72b1127fc7743997f6092727f187a6a621b
Frano Sincic <frano.sincic@gostudion.com>
2026-03-30 15:32:13 +0200

    fix: export missing grafana connections (#191)

--- src/components/grafana/index.ts
+++ src/components/grafana/index.ts
@@ -1,4 +1,9 @@
 export * as dashboard from './dashboards';
-export { GrafanaConnection, AMPConnection } from './connections';
+export {
+  GrafanaConnection,
+  AMPConnection,
+  CloudWatchLogsConnection,
+  XRayConnection,
+} from './connections';
 export { Grafana } from './grafana';
 export { GrafanaBuilder } from './builder';
