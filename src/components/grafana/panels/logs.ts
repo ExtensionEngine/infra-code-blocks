c2582258cc32947048184ac78b3704e793896e18
mandryllo <frano.sincic@gostudion.com>
2026-04-17 11:04:14 +0200

    refactor: add limit and search text variables

--- src/components/grafana/panels/logs.ts
+++ src/components/grafana/panels/logs.ts
@@ -1,52 +1,56 @@
 import { Panel } from './types';
-import {
-  createLogsPanel,
-  createTablePanel,
-  createTableForLogsPanel,
-} from './helpers';
+import { createTablePanel } from './helpers';
 
-export function createLogsListPanel(config: {
+export function createLogsViewPanel(config: {
   logGroupName: string;
   dataSourceName: string;
 }): Panel {
-  return createLogsPanel(
-    'Logs',
-    { x: 0, y: 0, w: 24, h: 10 },
-    config.dataSourceName,
-    config.logGroupName,
-    'fields @timestamp, @message, @logStream\n| sort @timestamp desc\n| limit 20',
-  );
-}
-
-export function createLogsListWithFiltersPanel(config: {
-  logGroupName: string;
-  dataSourceName: string;
-}): Panel {
-  return createTableForLogsPanel(
+  return createTablePanel(
     'Logs',
     { x: 0, y: 0, w: 24, h: 12 },
     config.dataSourceName,
     config.logGroupName,
-    `fields @timestamp\n| parse @message '"body":"*"' as body\n| parse @message '"res":{"statusCode":*}' as statusCode\n| parse @message '"severity_text":"*"' as logLevel\n| filter \${status_code}\n| filter \${log_level}\n| sort @timestamp desc\n| limit 20`,
-    {
-      id: 'organize',
-      options: {
-        renameByName: {
-          statusCode: 'Status Code',
-          logLevel: 'Log Level',
-          body: 'Body',
-          '@timestamp': 'Timestamp',
-        },
-        indexByName: {
-          '@timestamp': 0,
-          statusCode: 1,
-          logLevel: 2,
-          body: 3,
-        },
-        excludeByName: {
-          Value: true,
+    `fields @Timestamp
+    | parse @message '"body":"*"' as body
+    | parse @message '"res":{"statusCode":*}' as statusCode
+    | parse @message '"severity_text":"*"' as logLevel
+    | filter body like /\${search_text}/
+    | filter \${status_code}
+    | filter \${log_level}
+    | sort @timestamp desc
+    | limit \${limit}`,
+    [
+      {
+        id: 'organize',
+        options: {
+          renameByName: {
+            statusCode: 'Status Code',
+            logLevel: 'Log Level',
+            body: 'Body',
+            '@timestamp': 'Timestamp',
+          },
+          indexByName: {
+            '@timestamp': 0,
+            statusCode: 1,
+            logLevel: 2,
+            body: 3,
+          },
+          excludeByName: {
+            Value: true,
+          },
         },
       },
-    },
+      {
+        id: 'sortBy',
+        options: {
+          sort: [
+            {
+              field: 'Time',
+              desc: true,
+            },
+          ],
+        },
+      },
+    ],
   );
 }
--- src/components/grafana/variables/log-level.ts
+++ src/components/grafana/variables/log-level.ts
@@ -1,13 +1,19 @@
-import { Variable } from './types';
 import { createCustomVariable } from './helpers';
 
-export function createLogLevelVariable(): Variable {
-  return createCustomVariable('log_level', 'Log Level', [
-    { text: 'info', value: "logLevel = 'info'" },
-    { text: 'trace', value: "logLevel = 'trace'" },
-    { text: 'debug', value: "logLevel = 'debug'" },
-    { text: 'warn', value: "logLevel = 'warn'" },
-    { text: 'error', value: "logLevel = 'error'" },
-    { text: 'fatal', value: "logLevel = 'fatal'" },
-  ]);
+const LOG_LEVELS = [
+  { text: 'trace', value: "logLevel = 'trace'" },
+  { text: 'debug', value: "logLevel = 'debug'" },
+  { text: 'info', value: "logLevel = 'info'" },
+  { text: 'warn', value: "logLevel = 'warn'" },
+  { text: 'error', value: "logLevel = 'error'" },
+  { text: 'fatal', value: "logLevel = 'fatal'" },
+];
+
+export function createLogLevelVariable() {
+  return createCustomVariable(
+    'log_level',
+    'Log Level',
+    LOG_LEVELS,
+    LOG_LEVELS[2],
+  );
 }
